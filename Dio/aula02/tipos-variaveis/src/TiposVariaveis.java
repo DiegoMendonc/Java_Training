10037e7b3ab3d54a2c8f1ca46cfbf0f9196c7a12
DiegoMendonc <diego_felipe_g.m@live.com>
2024-11-25 12:30:43 -0300

    .

--- Dio/aula02/tipos-variaveis/src/TiposVariaveis.java
+++ Dio/aula02/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //double salarioMinimo = 2500;
+        //double atencaoNumero = 2.500; -> não é 2500 e sim 2,5
+        
+        //Variáveis podem ter sua alteração:
+
+        //Tipos primitivos
+
+        String meuNome = "fulano";
+        int numero = 5;
+        numero = 10;
+
+        System.out.println("\n" + numero + "\n");
+
+        //Constante não se alteram e sua escrita é em CAIXA ALTA;
+
+        final double VALOR_DE_PI = 3.14;
+
+        //VALOR_DE_PI = 3.10;
+
+        System.out.println(VALOR_DE_PI);
+
     }
 }
