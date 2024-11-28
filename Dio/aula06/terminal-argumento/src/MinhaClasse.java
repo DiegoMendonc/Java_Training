2874191bca74b9e69df78e40dd25ad49eee9a9a5
DiegoMendonc <diego_felipe_g.m@live.com>
2024-11-28 12:27:07 -0300

    aula06

---
+++ Dio/aula06/terminal-argumento/src/MinhaClasse.java
@@ -0,0 +1,10 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        
+        String linha = "-";
+
+        System.out.println("\n" + linha.repeat(50));
+        System.out.println("\n\nTeste em Terminal...\n\n");
+        System.out.println("\n" + linha.repeat(50));
+    }
+}
