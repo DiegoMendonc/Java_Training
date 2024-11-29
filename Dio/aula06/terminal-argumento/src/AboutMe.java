5c260b653ea2f5bfd37324c131083f719f335179
DiegoMendonc <diego_felipe_g.m@live.com>
2024-11-28 22:47:14 -0300

    .

--- Dio/aula06/terminal-argumento/src/AboutMe.java
+++ Dio/aula06/terminal-argumento/src/AboutMe.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) throws Exception {
         
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        Scanner scanner = new Scanner(System.in);
+        String linha = "-";
+        //String nome = args [0];
+        //String sobrenome = args [1];
+        //int idade = Integer.valueOf(args[2]);
+        //double altura = Double.valueOf(args[3]);
 
-        System.out.println("\nMe chamo " + nome + "" + sobrenome);
-        System.out.println("z\n Tenho " + idade + " anos");
-        System.out.println("\n E tenho " + altura + " de altura\n");
+        System.out.println("\n\nQual é o seu nome e sobrenome?");
+        String nome = scanner.next();
+
+        System.out.println("Qual é a sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Qual é sua altura? ");
+        double altura = scanner.nextDouble();
+        System.out.println("\n\n" + linha.repeat(50) + "\n");
+
+        System.out.println("Olá " + nome);
+        System.out.println("Você tem " + idade + " anos");
+        System.out.println("E possui uma altura de " + altura+ "cm");
+        
     }
 }
