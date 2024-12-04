7c7fa7dc09e40fb674bc12665ea03b3bf580b009
DiegoMendonc <diego_felipe_g.m@live.com>
2024-12-04 12:51:01 -0300

    projeto-codigo-aula09

---
+++ Dio/aula09/projeto-codigo/caso01/controle-candidatos/src/ProcessoSeletivo.java
@@ -0,0 +1,52 @@
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) throws Exception {
+        String linha = "_";
+
+        System.out.println(linha.repeat(60));
+        System.out.println("Processo seletivo\n");
+        //analisarCandidato(1899.0);
+        //analisarCandidato(2500.0);
+        //analisarCandidato(2000.0);
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos() {
+        String [] candidatos = {"FELIPE", "JOSÉ", "MARCIA", "PAULO", "HELIANA", "HELENA", "ROMUALDO", "BRUNO", "MIRELA", "DANIEL", "JONATAN", "DANIELA"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O cadidato " + candidato + " solicitou este valor de salário: R$" + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }
+        else if (salarioBase == salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        }
+        else {
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDAATOS");
+        }
+    }
+}
